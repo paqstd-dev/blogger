8ca5bb1bb00c4b952ef056d77476898b0821e611
paqstd <zebartcoc@gmail.com>
2023-01-12 04:26:47 +0300

    Added default (without env) client config const values

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
 
 // pagination
-export const PER_PAGE = import.meta.env.VITE_PER_PAGE;
+export const PER_PAGE = import.meta.env.VITE_PER_PAGE ?? 9;
